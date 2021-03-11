5f528de47ca8c1a2130c527faf4ff2a1e68ba4e1
mfoydl-cs <mfoydl@cs.stonybrook.edu>
2021-03-11 02:37:43 -0500

    precinct tile loading

--- js/map.js
+++ js/map.js
@@ -97,7 +97,6 @@ function resetHighlight(e, style) {
 }
 
 function highlightDistrict(district) {
-    console.log("hi")
     district.setStyle(highLightStyle);
 }
 
@@ -200,10 +199,26 @@ function getGeoJSON(stateAbbr) {
     var counties = L.geoJson(window["" + stateAbbr + "_COUNTY_20"], {
         style: countyStyle
     });
-
+    /*
     var precincts = L.geoJson(window["" + stateAbbr + "_VTD_20"], {
         style: precinctStyle
+    });*/
+    var tileIndex = geojsonvt(window["" + stateAbbr + "_VTD_20"],{
+        maxZoom: 12,  // max zoom to preserve detail on; can't be higher than 24
+        tolerance: 10, // simplification tolerance (higher means simpler)
+        extent: 4096, // tile extent (both width and height)
+        buffer: 64,   // tile buffer on each side
+        debug: 0,     // logging level (0 to disable, 1 or 2)
+        lineMetrics: false, // whether to enable line metrics tracking for LineString/MultiLineString features
+        promoteId: null,    // name of a feature property to promote to feature.id. Cannot be used with `generateId`
+        generateId: false,  // whether to generate feature ids. Cannot be used with `promoteId`
+        indexMaxZoom: 5,       // max zoom in the initial tile index
+        indexMaxPoints: 100000 // max number of points per tile in the index
     });
+    var precincts = L.gridLayer.precincts();
+    precincts.setTileIndex(tileIndex);
+
+
     return {
         stateJSON: stateJSON,
         counties: counties,
@@ -244,7 +259,7 @@ $(document).ready(function () {
     map = L.map('map');
 
     L.tileLayer('https://api.mapbox.com/styles/v1/mfoydl/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWZveWRsIiwiYSI6ImNrbGNqdnNocDBpZ2Qyd214bDZ2Y2piMDgifQ.nxwFI-kYDMC7ag_O8PgNhg', {
-        maxZoom: 18,
+        maxZoom: 12,
         attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, ' +
             'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
         id: 'cklh2icm3065v17qfbaanb9fe',
@@ -255,6 +270,8 @@ $(document).ready(function () {
     map.fitBounds(countryBounds);
     countryZoom = map.getZoom();
     map.setMinZoom(countryZoom);
+    console.log(map.getZoom())
+
 
     states.forEach(addStates);
 
@@ -273,10 +290,74 @@ $(document).ready(function () {
 
     center = L.control.center({ position: 'topleft' }).addTo(map);
 
+    map.on('zoom',function(){
+        console.log(map.getZoom())
+    })
+
 });
 
 function test(obj) {
     return obj.state.eachLayer(function (layer) { layer.setStyle(highLightStyle); console.log(layer) });
+}
+
+L.GridLayer.Precincts = L.GridLayer.extend({
+    createTile: function (coords,done) {
+        //var ctx = canvas.getContext('2d');
+        var error;
+
+        var leafTile = L.DomUtil.create('canvas', 'leaflet-tile');
+
+        var size = this.getTileSize();
+        leafTile.width = size.x;
+        leafTile.height = size.y;
+
+        var ctx = leafTile.getContext('2d');
+        var tile = this.tileIndex.getTile(coords.z, coords.x, coords.y);
+        if(!tile){
+            return leafTile;
+        }
+        ctx.clearRect(0, 0, leafTile.width, leafTile.height);
+        var features = tile.features;
+        ctx.strokeStyle = 'grey';
+        pad=0
+        for (var i = 0; i < features.length; i++) {
+            var feature = features[i],
+                type = feature.type;
+            ctx.beginPath();
+
+            for (var j = 0; j < feature.geometry.length; j++) {
+                var geom = feature.geometry[j];
+
+                if (type === 1) {
+                    ctx.arc(geom[0] * ratio + pad, geom[1] * ratio + pad, 2, 0, 2 * Math.PI, false);
+                    continue;
+                }
+
+                for (var k = 0; k < geom.length; k++) {
+                    var p = geom[k];
+                    var extent = 4096;
+
+                    var x = p[0] / extent * 256;
+                    var y = p[1] / extent * 256;
+                    if (k) ctx.lineTo(x + pad, y + pad);
+                    else ctx.moveTo(x + pad, y + pad);
+                }
+            }
+
+            ctx.stroke();
+        }
+        setTimeout(function () {
+            done(error, tile);
+        }, 1000);
+        return leafTile;
+    },
+    setTileIndex(tileIndex){
+        this.tileIndex=tileIndex
+    }
+});
+
+L.gridLayer.precincts = function (opts) {
+    return new L.GridLayer.Precincts(opts);
 }
 
 
